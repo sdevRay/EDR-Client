e120171d9a5bf3f879083c21d23ee43de2d5e3cc
Eleven Fifty <elevenfifty@Elevens-MacBook-Air.local>
2018-07-23 14:03:22 -0400

    deleting some boiler plate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './landing-page/sign-up/sign-up.component';
 import { LoginComponent } from './landing-page/login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EventsComponent } from './dashboard/events/events.component';
 import { BaseFitnessComponent } from './dashboard/base-fitness/base-fitness.component';
+
 
 @NgModule({
   declarations: [
