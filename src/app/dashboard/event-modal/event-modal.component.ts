c55afeb38d3f0f7e80631f2aea34fcf8d38e97fa
marreynolds <marreynolds13@gmail.com>
2018-07-25 20:15:25 -0400

    got modals working

--- src/app/dashboard/event-modal/event-modal.component.ts
+++ src/app/dashboard/event-modal/event-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms"
 
 @Component({
   selector: 'app-event-modal',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventModalComponent implements OnInit {
 
-  constructor() { }
+  private eventForm: FormGroup
+
+  constructor(private form: FormBuilder) { 
+
+    this.createForm()
+
+  }
+
+
 
   ngOnInit() {
   }
 
+
+  createForm(): void {
+    this.eventForm = this.form.group({
+      date: new FormControl,
+      disipline: new FormControl,
+      measurement: new FormControl,
+      unit: new FormControl,
+      currentDistance: new FormControl,
+      currentHours: new FormControl,
+      currentMinutes: new FormControl,
+      currentSeconds: new FormControl
+
+    })
+  }
 }
