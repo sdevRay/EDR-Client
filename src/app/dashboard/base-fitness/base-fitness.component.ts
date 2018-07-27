37ed4573af7d7d8990344e9f88de8793b5cc5137
marreynolds <marreynolds13@gmail.com>
2018-07-27 10:49:23 -0400

    added 404

--- src/app/dashboard/base-fitness/base-fitness.component.ts
+++ src/app/dashboard/base-fitness/base-fitness.component.ts
@@ -14,17 +14,6 @@ export class BaseFitnessComponent implements OnInit {
   constructor(public dialog: MatDialog, private dashboardService: DashboardService) { }
 
   openDialog(): void {
-    const dialogConfig = new MatDialogConfig;
-    dialogConfig.data = {
-      date: '',
-      discipline: '',
-      measurement: '',
-      unit: '',
-      currentDistance: Number,
-      currentHours: Number,
-      currentMinutes: Number,
-      currentSeconds: Number,
-    }
     let dialogRef = this.dialog.open(FitnessModalComponent, {
       height: '40em',
       width: '40em',
