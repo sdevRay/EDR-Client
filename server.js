66f3cdc192a7557d89e39d4d2c7615193f587a76
sdevRay <37095603+sdevRay@users.noreply.github.com>
2018-07-31 17:08:00 -0400

    stackoverflow

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '../dist/edrclient'));
+app.use(express.static(__dirname + '/dist/EDRClient'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '../dist/edrclient/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/EDRClient/index.html'));
 });
 
 // default Heroku port
